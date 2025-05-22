d99c3c068b1ce16e505ecfb725918923cb032baf
NishDevan <danishputradevananda1@gmail.com>
2025-05-22 07:52:55 +0700

    main.c apdet

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
-
+    int opsi = 0;
+    printf("Selamat Datang dalam Program Foodlink!\n");
+    printf("Menu Utama\n");
+    
+    do {
+        printf("1. Tambah Donatur\n2. Tambah Penerima\n3. Lihat Semua Data\n4. Cocokkan Donasi\n5. Tampilkan Laporan Donasi\n");
+        printf("Pilihlah Opsi yang Anda ingin Lakukan (1-5) : ");
+        scanf("%d", &opsi);
+        switch (opsi) {
+            case 1:
+                printf("1");
+                break;
+            case 2:
+                printf("2");
+                break;
+            case 3:
+                printf("3");
+                break;
+            case 4:
+                printf("4");
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Tolong Pilihlah Opsi yang sesuai!\n");
+                break;
+        }
+    } while(opsi != 5);
     return 0;
 }
